4664ca990469cb4da5d26d6e6418ffccc05d0576
damarendra <damarendran@gmail.com>
2021-02-03 09:50:14 +0700

    Finished Chapter 2

---
+++ app/src/main/java/com/hfad/messenger/ReceiveMessageActivity.java
@@ -0,0 +1,20 @@
+package com.hfad.messenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ReceiveMessageActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "message";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive_message);
+        TextView textView = findViewById(R.id.message);
+        textView.setText(getIntent().getStringExtra(EXTRA_MESSAGE));
+    }
+}
